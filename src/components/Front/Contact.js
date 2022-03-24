427395a95bff7b9ee32e715147a5d0bca6f87ed6
Grant Watson <gwatson117@gmail.com>
2022-03-23 20:39:36 -0600

    So many updates
    Working on trying to clear form onClick of Submitting form.

--- src/components/Front/Contact.js
+++ src/components/Front/Contact.js
@@ -1,55 +1,91 @@
-import React, { useState } from 'react'
+import React, { Component } from 'react'
 import emailjs from 'emailjs-com'
-import { Col } from 'react-bootstrap';
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
-export default function Contact() {
+class Contact extends Component {
+  notify = () => toast.success("Email has been sent!");
 
-  function sendEmail(e) {
+  sendEmail = (e) => {
     e.preventDefault();
-
+    
     emailjs.sendForm('service_libv1nq', 'template_qbxfxzc', e.target, 'QUnmkZs8gBchyu6ck')
       .then((result) => {
           console.log(result.text);
       }, (error) => {
           console.log(error.text);
       });
+    this.setState(() => this.initialState)
   }
-
-  return (
-    <div>
-      <br />
-      <br />
-      <hr />
-      <center>
-        <div className='card'>
-        <h1>Contact Form</h1>
-        <p>
-          Use the form below to share your questions, ideas, comments and feedback
-        </p>
-        <form id="contact-form" role="form" onSubmit={sendEmail}>
-          <div class="row">
-            <div class="col-md-12">
-              <div class="form-group">
-                <input id="form_name" type="text" name="from_name" class="form-control" placeholder="Please enter your firstname *" required="required" data-error="Firstname is required." />
+  render() {
+    return (
+      <div>
+        <br />
+        <br />
+        <hr />
+        <center>
+          <div className='card'>
+            <h1>Contact Form</h1>
+            <p>
+              Use the form below to share your questions, ideas, comments and feedback
+            </p>
+            <form id="contact-form" role="form" onSubmit={this.sendEmail}>
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="form-group">
+                    <input
+                      id="form_name"
+                      type="text"
+                      name="from_name"
+                      className="form-control"
+                      placeholder="Please enter your first and last name *"
+                      required="required"
+                      data-error="Name is required."
+                    />
+                  </div>
+                  <div className="form-group">
+                    <input
+                      id="form_email"
+                      type="email"
+                      name="email"
+                      className="form-control"
+                      placeholder="Please enter your email *"
+                      required="required"
+                      data-error="Valid email is required."
+                    />
+                  </div>
+                </div>
               </div>
-              <div class="form-group">
-                <input id="form_email" type="email" name="email" class="form-control" placeholder="Please enter your email *" required="required" data-error="Valid email is required." />
-              </div>
-            </div>
-          </div>
         
-          <div class="row">
-            <div class="col-md-12">
-              <textarea id="form_message" name="message" class="form-control" placeholder="Write your message here." rows="4" required="required" data-error="Please, leave us a message."></textarea> 
-            </div>
+              <div className="row">
+                <div className="col-md-12">
+                  <textarea
+                    id="form_message"
+                    name="message"
+                    className="form-control"
+                    placeholder="Write your message here."
+                    rows="6" required="required"
+                    data-error="Please, leave us a message.">
+                  </textarea>
+                </div>
+              </div>
+              <br />
+              <div className="row">
+                <div className="col-md-12">
+                  <input
+                    type="submit"
+                    className="btn btn-success btn-send pt-2 btn-block "
+                    onClick={this.notify}
+                    value="Send Message" />
+                </div>
+              </div>
+          
+            </form>
           </div>
-          <br />
-          <div class="col-md-12">
-              <input type="submit" class="btn btn-success btn-send pt-2 btn-block " value="Send Message"/>
-            </div>
-        </form>
-        </div>
-      </center>
-    </div>
-  );
+        </center>
+        <ToastContainer />
+      </div>
+    )
+  }
 }
+export default Contact
