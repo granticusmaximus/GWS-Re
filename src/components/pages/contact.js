c20633ed5e8947f5ac8f458e3b8b4ae27c39ebaf
Grant Watson <gwatson117@gmail.com>
2022-05-03 21:57:43 -0600

    Update to Contact Page

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,97 +1,80 @@
-import React from 'react'
-import emailjs from 'emailjs-com'
-import { ToastContainer, toast } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
-import { Container } from 'react-bootstrap';
+import React from 'react';
+import axios from 'axios';
 
+class Contact extends React.Component {
 
-export default function Contact() {
-  const notify = () => toast.success("Email has been sent!");
-  const nodFunct = async () => {
-    await notify(5000);
-    console.log("Reloading Page with interesting features, please hold");
-    window.location.reload(false);
-  };
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    emailjs.sendForm('service_libv1nq', 'template_qbxfxzc', e.target, 'QUnmkZs8gBchyu6ck', e)
-      .then((result) => {
-        console.log(result.text);
-      }, (error) => {
-        console.log(error.text);
-      });
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      message: ''
+    }
   }
 
-  return (
-    <div>
-      <br />
-      <br />
-      <hr />
-      <center>
-        <Container>
-          <div className='card'>
-            <h1>Contact Form</h1>
-            <p>
-              Use the form below to share your questions, ideas, comments and feedback
-            </p>
-            <form id="contact-form" role="form" onSubmit={sendEmail}>
-              <div className="row">
-                <div className="col-md-12">
-                  <div className="form-group">
-                    <input
-                      id="form_name"
-                      type="text"
-                      name="from_name"
-                      className="form-control"
-                      placeholder="Please enter your first and last name *"
-                      required="required"
-                      data-error="Name is required."
-                    />
-                  </div>
-                  <div className="form-group">
-                    <input
-                      id="form_email"
-                      type="email"
-                      name="email"
-                      className="form-control"
-                      placeholder="Please enter your email *"
-                      required="required"
-                      data-error="Valid email is required."
-                    />
-                  </div>
-                </div>
-              </div>
+  handleSubmit(e){
+    e.preventDefault();
+    axios({
+      method: "POST",
+      url:"https://www.grantwatson.dev/send",
+      data:  this.state
+    }).then((response)=>{
+      if (response.data.status === 'success') {
+        alert("Message Sent.");
+        this.resetForm()
+      } else if (response.data.status === 'fail') {
+        alert("Message failed to send.")
+      }
+    })
+  }
 
-              <div className="row">
-                <div className="col-md-12">
-                  <textarea
-                    id="form_message"
-                    name="message"
-                    className="form-control"
-                    placeholder="Write your message here."
-                    rows="6" required="required"
-                    data-error="Please, leave us a message.">
-                  </textarea>
-                </div>
-              </div>
-              <br />
-              <div className="row">
-                <div className="col-md-12">
-                  <input
-                    type="submit"
-                    className="btn btn-primary btn-send pt-2 btn-block "
-                    onClick={nodFunct}
-                    value="Send Message" />
-                </div>
-              </div>
+  resetForm(){
+    this.setState({
+      name: '',
+      email: '',
+      message: ''
+    })
+  }
 
-            </form>
-          </div>
-        </Container>
-      </center>
-      <ToastContainer />
-    </div>
-  );
+  render() {
+    return (
+      <div className='container'>
+        <div className='card'>
+          <center>
+            <h2>Contact Grant Watson Today!</h2>
+          </center>
+          <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
+            <div className="form-group">
+                <label htmlFor="name">Name</label>
+                <input type="text" className="form-control" id="name" value={this.state.name} onChange={this.onNameChange.bind(this)} />
+            </div>
+            <div className="form-group">
+                <label htmlFor="exampleInputEmail1">Email address</label>
+                <input type="email" className="form-control" id="email" aria-describedby="emailHelp" value={this.state.email} onChange={this.onEmailChange.bind(this)} />
+            </div>
+            <div className="form-group">
+                <label htmlFor="message">Message</label>
+                <textarea className="form-control" rows="5" id="message" value={this.state.message} onChange={this.onMessageChange.bind(this)} />
+            </div>
+            <button type="submit" className="btn btn-primary">Submit Message</button>
+          </form>
+        </div>
+      </div>      
+      
+    );
+  }
+
+  onNameChange(event) {
+	  this.setState({name: event.target.value})
+  }
+
+  onEmailChange(event) {
+	  this.setState({email: event.target.value})
+  }
+
+  onMessageChange(event) {
+	  this.setState({message: event.target.value})
+  }
 }
+
+export default Contact;
